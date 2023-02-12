794bfab1dec11447db1a150f040f838979faf42c
VishariBeduk <Vishari.Beduk@outlook.com>
2023-02-12 18:56:48 +0000

    MS-SQL iso sqlite

---
+++ All/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+EntityCaching.Example();
+
+InjectLogger.Example();
+
+OptimisticConcurrencyInterception.Example();
+
+QueryInterception.Example();
+
+SimpleMaterialization.Example();
